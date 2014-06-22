c31484a92baa2ce40c6ef988c3c49681d11aa76d
Maciej Małecki <me@mmalecki.com>
2014-06-23 00:21:33 +0200

    Attribute error messages

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = function (name, code, signal) {
   if (!name) throw new Error('`name` is required')
   if (!code && !signal) return null
 
+  // Error messages stolen from https://github.com/nathan7/spawn-write-stream/blob/4549bb8bae173a00b4c885ee2a3dc085b5ffe51b/index.js#L40-L42.
   var err = signal
     ? new Error('`' + name + '` killed by signal `' + signal + '`')
     : new Error('`' + name + '` exited with ' + code)
